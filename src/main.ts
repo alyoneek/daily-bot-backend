c727bd36f903b98da4a333894d7a681664b23221
alyoneek <alyonta03@mail.ru>
2023-06-30 10:06:05 +0700

    fix CORS problem

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
