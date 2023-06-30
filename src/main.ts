802b59f36ab5212a086ee711a5d074515f1d368e
alyoneek <alyonta03@mail.ru>
2023-06-30 12:48:40 +0700

    Normalize response

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as mongoose from 'mongoose';
 import { AppModule } from './app.module';
 
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
+  mongoose.set('toJSON', {
+    virtuals: true,
+    transform: (doc, converted) => {
+      delete converted._id;
+      delete converted.__v;
+    },
+  });
+
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
